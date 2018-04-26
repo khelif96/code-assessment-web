de782280390a161814f9c6534bd795f92523abd6
khelif96 <khelif96@gmail.com>
2018-04-26 14:56:28 -0400

    Change Product Text and styling to correspond with desired output for individual product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-
+import styles from './Product.css'
 const Product = ({ price, inventory, title }) => (
-  <div>
-    {title} - &#36;{price}{inventory ? ` x ${inventory}` : null}
+  <div className={styles.Product}>
+    <div className={styles.ProductTopRow}>
+      <h3 className={styles.ProductTitle}>{title}</h3>
+      <h4 className={styles.ProductPrice}> &#36;{price}</h4>
+    </div>
+    <p className={styles.ProductInv}>{inventory ? `${inventory} Remaining` : null}</p>
   </div>
 )
 

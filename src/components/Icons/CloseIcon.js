371b4474683bec27d08076ec73a1cff047840693
khelif96 <khelif96@gmail.com>
2018-04-30 00:34:10 -0400

    Generated Necessary Icons for cart

---
+++ src/components/Icons/CloseIcon.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const closeIcon = props => (
+    <div>
+        <svg width={props.width} height={props.height} viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            {/* <desc>Created with Sketch.</desc> */}
+            <defs></defs>
+            <g id="v2" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd" strokeLinecap="round">
+                <g id="Desktop-Copy-2" transform="translate(-971.000000, -205.000000)" stroke={props.stroke} strokeWidth="2">
+                    <g id="Group-3" transform="translate(972.000000, 206.000000)">
+                        <path d="M0.714285714,0.714285714 L29.6582851,29.6582851" id="Line-3"></path>
+                        <path d="M28,1 L0,30" id="Line-3"></path>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    </div>
+);
+
+export default closeIcon
